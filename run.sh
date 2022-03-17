59b715f129177ed9c3ae6006ce486dbcbce73de1
ryanflo17 <quantizedflo@gmail.com>
2022-03-17 19:17:01 +0100

    build env

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 pip install virtualenv
 python3 -m virtualenv pem-ml-env
 source pem-ml-env/bin/activate
 pip3 install mlagents
-/home/r/riessf/.local/bin/mlagents-learn ./config/Volleyball.yaml --env=./competitive-env/RobotVolleyBallEnvironment.x86_64 --run-id=competitive-six-joins-run-1
+mlagents-learn ./config/Volleyball.yaml --env=./competitive-env/RobotVolleyBallEnvironment.x86_64 --run-id=run-1
